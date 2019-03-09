df7fbaa0e3c2b3691da17a3127e3870f1d43b342
shohib pathan <pathan@shohibs-macbook-air.local>
2019-03-09 17:20:21 +0530

    initial commit

---
+++ ExpandableTableView/ExpandCell.swift
@@ -0,0 +1,13 @@
+//
+//  ExpandCell.swift
+//  ExpandableTableView
+//
+//  Created by shohib mohammed pathan on 09/03/19.
+//  Copyright © 2019 AppKrafting. All rights reserved.
+//
+
+import UIKit
+
+class ExpandCell:UITableViewCell{
+    @IBOutlet weak var expandLbl: UILabel!
+}
